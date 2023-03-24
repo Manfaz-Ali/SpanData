e3fc61caf18428737519046b20309d3ad0974be2
Manfaz-Ali <125426373+Manfaz-Ali@users.noreply.github.com>
2023-03-24 07:54:00 +0500

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,14 @@
+d1 = {'madrid':     'https://es.wallapop.com/app/search?category_ids=14000&filters_source=quick_filters&longitude=-3.69196&latitude=40.41956&min_sale_price=800&order_by=price_low_to_high',
+      'barcelona':  'https://es.wallapop.com/app/search?category_ids=14000&latitude=41.394563&longitude=2.170814&filters_source=quick_filters&min_sale_price=800&order_by=price_low_to_high',
+      'valencia':   'https://es.wallapop.com/app/search?category_ids=14000&latitude=39.441767&longitude=-0.396028&filters_source=quick_filters&min_sale_price=800&order_by=price_low_to_high',
+      'sevella':    'https://es.wallapop.com/app/search?category_ids=14000&latitude=37.384697&longitude=-5.989868&filters_source=quick_filters&min_sale_price=800&order_by=price_low_to_high',
+      'zaragoza':   'https://es.wallapop.com/app/search?category_ids=14000&latitude=41.649946&longitude=-0.912819&filters_source=quick_filters&min_sale_price=800&order_by=price_low_to_high',
+      'malaga':     'https://es.wallapop.com/app/search?category_ids=14000&latitude=36.72108&longitude=-4.421041&filters_source=quick_filters&min_sale_price=800&order_by=price_low_to_high',
+      'alicante':   'https://es.wallapop.com/app/search?category_ids=14000&latitude=38.35782&longitude=-0.5425641&filters_source=quick_filters&min_sale_price=800&order_by=price_low_to_high',
+      'bilbao':     'https://es.wallapop.com/app/search?category_ids=14000&latitude=43.262964&longitude=-2.934948&filters_source=quick_filters&min_sale_price=800&order_by=price_low_to_high',
+
+
+
+
+
+      }
